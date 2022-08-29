36062b1037ecbcdc8d6dafb5c75680effa265630
chetan1120 <er.chetanchawla@gmail.com>
2022-08-28 23:29:48 -0400

    added ml model code 3

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("all ml model code")
+def model1:
+    print("addng code for ml model1")
+
+
+def model2:
+    print("addng code for ml model2")
+
+def model3:
+    print("addng code for ml model3")
